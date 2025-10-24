f8b0157be43ebab8f3f547ff9075c593ad211c73
Ahmad Alifariz Anwar <hanaaraashi@gmail.com>
2025-10-24 15:24:35 +0800

    Menambah update lampu di controller

--- app/Http/Controllers/LampController.php
+++ app/Http/Controllers/LampController.php
@@ -13,4 +13,42 @@ class LampController extends Controller
         
         return view('lamps.index', compact('lamps'));
     }
+
+    public function updateLamp($id)
+    {
+        //find lamp
+        $lamp = Lamp::findOrFail($id);
+
+        //update lamp
+        if($lamp -> status == 1) {
+            
+            //update lamp
+            $lamp->update(['status' => 0]);
+
+            //insert history
+            $lamp->histories()->create(['status' => 'OFF']);
+
+            //return response
+            return response()->json ([
+                'success' => true,
+                'message' => $lamp->name . 'Berhasil Dimatikan!',
+                'status' => $lamp
+            ]);
+        } else if ($lamp->status == 0) {
+            
+            //update lamp
+            $lamp->update(['status' => 1]);
+
+            //insert history
+            $lamp->histories()->create(['status' => 'ON']);
+
+            //return response
+            return response()->json ([
+                'success' => true,
+                'message' => $lamp->name . 'Berhasil Dihidupkan!',
+                'status' => $lamp
+            ]);
+        }
+    }
+
 }
