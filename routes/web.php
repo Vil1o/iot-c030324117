dc5594bad7d7df5280a678d3458eae4c30e52317
Ahmad Alifariz Anwar <hanaaraashi@gmail.com>
2025-10-24 15:28:40 +0800

    Membuat route update lamp

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\LampController::class, 'index'])->name('lamps.index');
+
+//update lamp
+Route::post('/lamps/{id}', [App\Http\Controllers\LampController::class, 'updateLamp'])->name('lamps.update');
